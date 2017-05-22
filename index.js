271833cbb46162bbfa92672d89d971f0d15d0842
titipakorn <titipakorn.p@gmail.com>
2017-05-22 10:33:24 +0700

    new

--- index.js
+++ index.js
@@ -395,7 +395,7 @@ bot.on('message', function(event) {
                     {
                       postsRef.update({
                         step: `F${parseInt(event.message.text[1])+1}`,
-                        temp: Object.assign(user.temp,{time: f1match[event.message.text[1]][msg],timestamp:firebase.database.ServerValue.TIMESTAMP})
+                        temp: Object.assign(user.temp,{time: f1match[event.message.text[1]][msg],timestamp:moment().valueOf()})
                       })
                       confirmtemplate(event, `F${parseInt(user.step[1])+1}`, `Source: ${user.temp.source} in next ${f1ans[event.message.text[1]][msg]}, Destination: ${user.temp.destination}`,'',1)
                       break
